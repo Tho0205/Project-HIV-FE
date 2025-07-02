c7e7bf6d9da50c567d8d34d89cc76bfbe6107e42
Tho0205 <tho02052004@gmail.com>
2025-07-03 05:07:32 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -44,7 +44,12 @@ export const routes = [
   { path: "/Appointment-History", element: <AppointmentHistory />},
   { path: "/Docter-MedicalRecord", element: <DoctorMedicalRecordPage/>},
   { path: "/Patient-MedicalRecord", element: <PatientMedicalRecordPage/>},
+<<<<<<< Updated upstream
   { path: "/Protocol-management", element: <ARVProtocolManagement /> },
   {path: "/Appointment-History", element: <AppointmentHistory />},
   { path: "/DashBoard", element: <Dashboard />}
+=======
+  { path: "/DashBoard", element: <Dashboard />}
+
+>>>>>>> Stashed changes
 ];
